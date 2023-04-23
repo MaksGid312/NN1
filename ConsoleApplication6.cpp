b482be2a525ee50054b66ec2cc5a9dbf1b5f052e
MaksGid <maksimgid@yandex.ru>
2023-04-23 18:32:22 +0300

    first commit

---
+++ ConsoleApplication6.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream> 
+#include <string> 
+
+using namespace std;
+
+int sum(int a, int b) {
+    return a + b;
+}
+
+int mult(int a, int b) {
+    return a * b;
+}
+int main() {
+    cout << "Enter 2 numbers: ";
+    int a, b;
+    cin >> a >> b;
+    cout << sum(a, b) << endl;
+    cout << mult(a, b) << endl;
+    system("pause");
+    return 0;
+}
